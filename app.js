2a20d950aaa386d60423310f2a3cb58d5ef15f19
taddeimania <jtaddei@gmail.com>
2017-07-13 09:14:43 -0400

    forgot to commit previous day, almost done

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const mongoURL = require('./config.json')[env].mongoURL;
 mongoose.connect(mongoURL);
 
 const createUser = (username, password) => {
-  return User.create({username: username, password: password});
+  return User.create({username: username, password: createPasswordHashObject(password)});
 };
 
 const createPasswordHashObject = (password, salt="") => {
   salt = salt || crypto.randomBytes(Math.ceil(32 * 3 / 4)).toString('base64').slice(0, 8);
   const hash = crypto.pbkdf2Sync(password, salt, 100, 256, 'sha256');
-  return hash.toString("base64");
+  const hashString = hash.toString("base64");
+  return {salt: salt, iterations: 100, hash: hashString};
 };
 // write function that takes password and returns object with details
 
--- tests/modelTests.js
+++ tests/modelTests.js
@@ -6,9 +6,10 @@ const expect = require('chai').expect;
 describe('user model tests', () => {
 
   it('can generate a password object from password string', (done) => {
-    const passwordObject = createPasswordHashObject("peanut");
+    const passwordObject = createPasswordHashObject("peanut", "a");
+    const expectedHashObject = {salt: "a", iterations: 100, hash: "jnB+rZt2ved0J0R47XXaTQJl+UJE2fJljqsZTIMKJ1qdMaDcC5aOCZtpCPYBySyyvmEDpt55XTYBbYW+UFAg6LF7bkDGVPPu2YDch3nmYrB+5iGF+EVYxaqKWFdTUyGV/AGogfaUvjKnROFy1JIhF1G4g1B5a1SgA2dqYQeVeOOktpYC8vV3BVDgNpohjM0bXvCl+fQ2zxY7c8UwQ+/ofnW3ou/LWz9eKYzQTVLCJdgqWO6oSE+KRFlK3JZaE2HrczHVr6lsY/Y+XVMM8E0zkIsG0pebd55uj11DrygebeleYPBBajBcN8atyFhs0c0YU98ATrddcOplFLLwBCI2Qw=="};
     expect(passwordObject).to.not.equal(null);
-    expect(passwordObject).to.equal("");
+    expect(passwordObject).to.deep.equal(expectedHashObject);
     done();
   });
 
@@ -16,6 +17,7 @@ describe('user model tests', () => {
     createUser("username", "password").then(user => {
       expect(user.username).to.equal("username");
       expect(user.password).to.be.an("object");
+      expect(user.password.hash.length).to.equal(344);
       done();
     });
   });
